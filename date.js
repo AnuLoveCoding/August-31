5aedd1447d241b117e8bd64895adb58aeec02fa9
anuLoveCoading <anusrivas07@gmail.com>
2022-09-01 08:38:36 +0530

    date

---
+++ date.js
@@ -0,0 +1,15 @@
+let date =new Date()
+console.log(date[Symbol.toPrimitive]('string'))
+
+let name ="anurag"
+console.log(name.toUpperCase());
+
+
+let raj="ANURAG";
+console.log(raj.toLowerCase())
+
+
+
+
+let aaj= new Date();
+console.log(aaj[Symbol.toPrimitive]('string'))
